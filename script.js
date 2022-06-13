75bc0170071f496eeb5a29212c1720841a114367
Michel Schaukoski <92960055+mijeus@users.noreply.github.com>
2022-06-13 11:32:44 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+//variaveis?
+//dados de entrada?
+//dados de saída?
+
+const elementoResposta = document.querySelector('#resposta')
+
+const inputPergunta = document.querySelector('#pergunta')
+
+const buttonPerguntar = document.querySelector('#buttonPerguntar')
+
+const respostas = [
+  'Certeza!',
+  'Não tenho tanta certeza.',
+  'É decididamente assim.',
+  'Não conte com isso.',
+  'Sem dúvidas!',
+  'Pergunte novamente mais tarde.',
+  'Sim, definitivamente!',
+  'Minha resposta é não.',
+  'Você pode contar com isso.',
+  'Melhor não te dizer agora.',
+  'A meu ver, sim.',
+  'Minhas fontes dizem não.',
+  'Provavelmente.',
+  'Não é possível prever agora.',
+  'Perspectiva boa.',
+  'As perspectivas não são tão boas.',
+  'Sim.',
+  'Concentre-se e pergunte novamente.',
+  'Sinais apontam que sim.'
+]
+
+function fazerPergunta() {
+  if (inputPergunta.value == '') {
+    alert('Digite sua pergunta!')
+    return
+  }
+
+  buttonPerguntar.setAttribute('disabled', true)
+
+  const pergunta = '<div>' + inputPergunta.value + '</div>'
+
+  const totalRespostas = respostas.length
+  const numeroAleatorio = Math.floor(Math.random() * totalRespostas)
+
+  elementoResposta.innerHTML = pergunta + respostas[numeroAleatorio]
+
+  elementoResposta.style.opacity = 1
+
+  setTimeout(function () {
+    buttonPerguntar.removeAttribute('disabled')
+    elementoResposta.style.opacity = 0
+  }, 3000)
+}
